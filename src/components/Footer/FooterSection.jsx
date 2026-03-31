670d2fd1ce56c48022e2d86644c0411f1a2062d0
Prashant <prashanttkumar12@gmail.com>
2026-03-31 21:57:53 +0530

    deploying

--- src/components/Footer/FooterSection.jsx
+++ src/components/Footer/FooterSection.jsx
@@ -1,17 +1,22 @@
-import { memo } from 'react'
+import { memo } from "react";
 
 const Footer = memo(() => (
   <footer className="bg-yellow px-20 py-10 flex items-center justify-between">
-    <span className="font-bebas text-dark tracking-[2px]" style={{ fontSize: 36 }}>
-      ARYAN SHARMA.
+    <span
+      className="font-bebas text-dark tracking-[2px]"
+      style={{ fontSize: 36 }}
+    >
+      Prashant Kumar.
     </span>
     <div className="text-right text-[11px] font-semibold text-dark opacity-70 leading-[1.8]">
-      MERN Stack Developer<br />
-      Available for Freelance &amp; Full-time<br />
-      aryan@example.com
+      MERN Stack Developer
+      <br />
+      Available for Freelance &amp; Full-time
+      <br />
+      prashanttkumar12@gmail.com
     </div>
   </footer>
-))
+));
 
-Footer.displayName = 'Footer'
-export default Footer
+Footer.displayName = "Footer";
+export default Footer;
